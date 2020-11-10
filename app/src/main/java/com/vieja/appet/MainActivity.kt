712e5dfdb952f65f8f40c3a39cfa1bff24417a29
Vieja <mwiechcz@gmail.com>
2020-11-11 00:27:45 +0100

    add drawer layout navigation

--- app/src/main/java/com/vieja/appet/MainActivity.kt
+++ app/src/main/java/com/vieja/appet/MainActivity.kt
@@ -1,27 +1,109 @@
 package com.vieja.appet
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+        val navBottomView: BottomNavigationView = findViewById(R.id.nav_bottom_view)
+        val navDraverView: NavigationView = findViewById(R.id.nav_drawer_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
+        navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_care, R.id.navigation_info))
-        //setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+//        val appBarConfiguration = AppBarConfiguration(setOf(
+//                R.id.navigation_home, R.id.navigation_care, R.id.navigation_info))
+        appBarConfiguration = AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_care,
+            R.id.navigation_info, R.id.blankFragment) //Pass the ids of fragments from nav_graph which you d'ont want to show back button in toolbar
+            .setDrawerLayout(drawer_layout) //Pass the drawer layout id from activity xml
+            .build()
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+//        navBottomView.setupWithNavController(navController)
+
+        visibilityNavElements(navController)
 
     }
+
+    private fun visibilityNavElements(navController: NavController) {
+
+        //Listen for the change in fragment (navigation) and hide or show drawer or bottom navigation accordingly if required
+        //Modify this according to your need
+        //If you want you can implement logic to deselect(styling) the bottom navigation menu item when drawer item selected using listener
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.careCategoryFragment -> hideDrawerNavigation()
+//                R.id.settingsFragment -> hideBottomNavigation()
+                else -> showBothNavigation()
+            }
+        }
+
+    }
+
+    private fun hideBothNavigation() { //Hide both drawer and bottom navigation bar
+        nav_bottom_view?.visibility = View.GONE
+        nav_drawer_view?.visibility = View.GONE
+        drawer_layout?.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED) //To lock navigation drawer so that it don't respond to swipe gesture
+    }
+
+    private fun hideDrawerNavigation() { //Hide drawer navigation bar
+        nav_bottom_view?.visibility = View.VISIBLE
+        nav_drawer_view?.visibility = View.GONE
+        drawer_layout?.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED) //To lock navigation drawer so that it don't respond to swipe gesture
+    }
+
+    private fun hideBottomNavigation() { //Hide bottom navigation
+        nav_bottom_view?.visibility = View.GONE
+        nav_drawer_view?.visibility = View.VISIBLE
+        drawer_layout?.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED) //To unlock navigation drawer
+        nav_drawer_view?.setupWithNavController(navController) //Setup Drawer navigation with navController
+    }
+
+
+    private fun showBothNavigation() {
+        nav_bottom_view?.visibility = View.VISIBLE
+        nav_drawer_view?.visibility = View.VISIBLE
+        drawer_layout?.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+        setupNavControl() //To configure navController with drawer and bottom navigation
+    }
+
+    private fun setupNavControl() {
+        nav_drawer_view?.setupWithNavController(navController) //Setup Drawer navigation with navController
+        nav_bottom_view?.setupWithNavController(navController) //Setup Bottom navigation with navController
+    }
+
+    override fun onSupportNavigateUp(): Boolean { //Setup appBarConfiguration for back arrow
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+    }
+
+    override fun onBackPressed() {
+        when { //If drawer layout is open close that on back pressed
+            drawer_layout.isDrawerOpen(GravityCompat.START) -> {
+                drawer_layout.closeDrawer(GravityCompat.START)
+            }
+            else -> {
+                super.onBackPressed() //If drawer is already in closed condition then go back
+            }
+        }
+    }
+
+
 }
